fb53abd8f572fc62faadb407041d822b08899fb5
Kishan Bhatt <iamkishanbhatt@gmail.com>
2022-08-01 13:57:28 +0530

    Fixing pagination issue in get all products

--- app/Http/Controllers/Api/v1/Client/ProductController.php
+++ app/Http/Controllers/Api/v1/Client/ProductController.php
@@ -124,7 +124,7 @@ class ProductController extends Controller
 
             } else {
 
-                $data = DB::table('products')
+                $data = DB::table('products, variations, colors, images')
                             ->join('product_variations as variations', 'variations.product_id', '=', 'products.id')
                             ->join('product_images as images', 'images.product_id', '=', 'products.id')
                             ->join('product_colors as colors', 'colors.product_id', '=', 'products.id')
