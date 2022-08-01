3d9cc71c1fd289066ec6a659ddb6aaec52d1edd5
Kishan Bhatt <iamkishanbhatt@gmail.com>
2022-08-01 12:11:11 +0530

    Fixing pagination issue in get all products

--- app/Http/Controllers/Api/v1/Client/ProductController.php
+++ app/Http/Controllers/Api/v1/Client/ProductController.php
@@ -193,7 +193,7 @@ class ProductController extends Controller
 
             }
         }        
-        dd($products->all()->paginate(4));
+        dd($products->get()->paginate(4));
         $data = $products->paginate(4);
 
         $finalProducts = (!empty($data)) ? $data->toArray() : array();        
