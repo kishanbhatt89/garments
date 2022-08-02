b6ab45cfe8fdf772fdf19231f4c4c3a1938eb967
Kishan Bhatt <iamkishanbhatt@gmail.com>
2022-08-02 16:24:13 +0530

    Get all products api pagination fixing

--- app/Http/Resources/Api/v1/ProductResource.php
+++ app/Http/Resources/Api/v1/ProductResource.php
@@ -109,16 +109,16 @@ class ProductResource extends ResourceCollection
         }           
 
         $responseData['products'] = count($productData) > 0 ? $productData : [];                
-        dd($this);
+        
         $responseData['pagination'][] = [
             'current_page' => $this['current_page'],
-            'first_page_url' => $this['first_page_url'],
+            'first_page_url' => url('').$this['first_page_url'],
             'from' => $this['from'],
-            'next_page_url' => $this['next_page_url'],
+            'next_page_url' => url('').$this['next_page_url'],
             'per_page' => $this['per_page'],
-            'prev_page_url' => $this['prev_page_url'],            
+            'prev_page_url' => url('').$this['prev_page_url'],            
             'to' => $this['to'],
-            'last_page_url' => $this['last_page_url'],
+            'last_page_url' => url('').$this['last_page_url'],
             'total_products' => $this['total'],
             'last_page' => intval(explode('page=', $this['last_page_url'])[1]),
         ];            
