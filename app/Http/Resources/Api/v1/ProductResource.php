50e12f08cc95aaa3b457be696d7a650e326c3c4b
Kishan Bhatt <iamkishanbhatt@gmail.com>
2022-08-02 15:51:02 +0530

    Get all products api pagination fixing

--- app/Http/Resources/Api/v1/ProductResource.php
+++ app/Http/Resources/Api/v1/ProductResource.php
@@ -16,7 +16,7 @@ class ProductResource extends ResourceCollection
     {                
         $responseData = $productData = [];
 
-        dd($this);
+        dd($this['data']);
         
         if (isset($this['data']) && count($this['data']) > 0) {            
                     
