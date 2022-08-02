019bd05f5dc6f65b81bd54c651408eae83764401
Kishan Bhatt <iamkishanbhatt@gmail.com>
2022-08-02 14:34:59 +0530

    Get all products api pagination fixing

--- app/Http/Controllers/Api/v1/Client/ProductController.php
+++ app/Http/Controllers/Api/v1/Client/ProductController.php
@@ -147,8 +147,8 @@ class ProductController extends Controller
                         "status" => $product->status,
                         "created_at" => $product->created_at->diffForHumans(),
                         "updated_at" => $product->updated_at->diffForHumans(),
-                        "price" => $product->variations->sort('price')->first()->price,
-                        "discounted_price" => $product->variations->sort('price')->first()->discounted_price
+                        //"price" => $product->variations->sort('price')->first()->price,
+                        //"discounted_price" => $product->variations->sort('price')->first()->discounted_price
                     ];
 
                     if ($request->sort == 'price-htol') {
