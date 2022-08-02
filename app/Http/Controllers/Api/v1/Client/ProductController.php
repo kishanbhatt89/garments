1fa4624936e4ba796d73efdba055cdf7f75b5272
Kishan Bhatt <iamkishanbhatt@gmail.com>
2022-08-02 14:35:31 +0530

    Get all products api pagination fixing

--- app/Http/Controllers/Api/v1/Client/ProductController.php
+++ app/Http/Controllers/Api/v1/Client/ProductController.php
@@ -132,7 +132,7 @@ class ProductController extends Controller
                                 ->get();                
 
                 foreach ($products as $product) {
-                    
+                    dd($product->variations->sort('price')->first());
                     $productsArr[] = [
                         "id" => $product->id,
                         "client_id" => $product->client_id,
