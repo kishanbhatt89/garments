0158cd47e2a8e0e99ac84569498c6f64000c4e45
Kishan Bhatt <iamkishanbhatt@gmail.com>
2022-08-03 11:18:32 +0530

    Get all products pagination fixing

--- app/Http/Controllers/Api/v1/Client/ProductController.php
+++ app/Http/Controllers/Api/v1/Client/ProductController.php
@@ -323,7 +323,7 @@ class ProductController extends Controller
 
     public function store(ProductRequest $request) {
 
-        dd($request->variations);
+        dd(collect($request->variations)->sortBy('price')->first()->price, collect($request->variations)->sortBy('price')->first()->discounted_price);
 
         if (auth('client')->user()->is_store_setup == 0) {
             return response()->json([                
