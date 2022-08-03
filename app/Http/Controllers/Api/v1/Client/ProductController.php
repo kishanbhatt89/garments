0158dfc02f672bd76a98818e5d69f0a9cf9ad2aa
Kishan Bhatt <iamkishanbhatt@gmail.com>
2022-08-03 11:47:33 +0530

    Get all products pagination fixing

--- app/Http/Controllers/Api/v1/Client/ProductController.php
+++ app/Http/Controllers/Api/v1/Client/ProductController.php
@@ -362,7 +362,7 @@ class ProductController extends Controller
             $product->status = $request->status;
 
             if ($product->save()) {
-
+                dd($product);
                 if (!empty($request->colors)) {
                     foreach ($request->colors as $color) {
                         $productColor = new ProductColor();
@@ -383,11 +383,7 @@ class ProductController extends Controller
                         $productVariation->discounted_price = $variation['discounted_price'];
                         $productVariation->status = 'active';
                         $productVariation->save();
-                    }
-                    $product->fill([
-                        "price" => collect($request->variations)->sortBy('price')->first()['price'],
-                        "discounted_price" => collect($request->variations)->sortBy('price')->first()['discounted_price']
-                    ]);                    
+                    }                                        
                 }
 
                 return response()->json([                
